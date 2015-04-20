82d7bb95f1ec7424b284a7823d8506722d99c4a5
kazuya030 <kazuya030@gmail.com>
2015-04-20 19:09:58 +0900

    Add EulerPlus 02

--- src/EulerPlus.hs
+++ src/EulerPlus.hs
@@ -1,8 +1,11 @@
 module EulerPlus where
 
-import math
 import Control.Monad
 import Control.Applicative
+
+main = do
+    ns <-  fmap (read :: String -> Integer) . lines <$> getContents
+    mapM_ print $ map euler02 $ drop 1 ns
 
 sumTo :: Integer -> Integer
 sumTo n = n*(n+1) `div` 2
@@ -10,9 +13,11 @@ sumTo n = n*(n+1) `div` 2
 sumMultipleTo :: Integer -> Integer -> Integer
 sumMultipleTo n k = k * sumTo ( n `div` k )
 
-solve :: Integer -> Integer
-solve n =  sumMultipleTo (n-1)  3 + sumMultipleTo (n-1) 5 - sumMultipleTo (n-1) 15
+euler01 :: Integer -> Integer
+euler01 n =  sumMultipleTo (n-1)  3 + sumMultipleTo (n-1) 5 - sumMultipleTo (n-1) 15
 
-main = do
-    ns <-  fmap (read :: String -> Integer) . lines <$> getContents
-    mapM_ print $ map solve $ drop 1 ns
+fibs :: [Integer]
+fibs = 1:2:zipWith (+) fibs (tail fibs)
+
+euler02 :: Integer -> Integer
+euler02 n =  sum $ [ x | x <- takeWhile(<n) fibs, x `mod` 2==0]
